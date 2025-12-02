91c9edd7edfa3144d35ae0690decbf35938f4e92
MomenJaber01 <105093791+MomenJaber01@users.noreply.github.com>
2025-12-02 16:19:02 +0000

    test4

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+/** 
 let video;
 let label = "waiting...";
 let classifier;
@@ -5,8 +6,7 @@ let classifier;
 
 // STEP 1: Load the model!
 function preload() {
-  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/hR02g_emx/');
-  //classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/hR02g_emx/model.json');
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/hR02g_emx/model.json');
 }
 
 function setup() {
@@ -50,5 +50,63 @@ function gotResults(error, results) {
   label = results[0].label;
 }
 
+*/
 
+let label = "waiting...";
+
+// Classifier and model url
+let classifier;
+let modelURL = 'https://teachablemachine.withgoogle.com/models/hR02g_emx/';
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/hR02g_emx/model.json');
+}
+
+function setup() {
+  createCanvas(640, 520);
+
+  // STEP 2: Start classifying (will listen to mic by default)
+  classifyAudio();
+}
+
+// STEP 2 classify!
+function classifyAudio() {
+  classifier.classify(gotResults);
+}
+
+function draw() {
+  background(0);
+
+  // STEP 4: Draw the label
+  // textSize(32);
+  textAlign(CENTER, CENTER);
+  // fill(255);
+  // text(label, width/2, height - 16);
+
+  // Background noise is headphones
+  let emoji = "🎧";
+  // Pick an emoji based on label
+
+  if (label === "QUIET") {
+  emoji = "🤫";
+} else if (label === "YES") {
+  emoji = "✅";
+} else if (label === "NO") {
+  emoji = "❌";
+}
+  // Draw the emoji
+  textSize(256);
+  text(emoji, width / 2, height / 2);
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Store the label
+  label = results[0].label;
+}
 
